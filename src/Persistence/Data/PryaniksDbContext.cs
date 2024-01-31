8ca439ee81e818e70766bf38f9d4ebd32dc73970
RainDance74 <104539834+RainDance74@users.noreply.github.com>
2024-01-31 15:19:46 +0500

    Add DbContext

---
+++ src/Persistence/Data/PryaniksDbContext.cs
@@ -0,0 +1,22 @@
+﻿using System.Reflection;
+
+using Microsoft.EntityFrameworkCore;
+
+using ShopOfPryaniks.Persistence.Entities;
+
+namespace ShopOfPryaniks.Persistence.Data;
+
+public class PryanikiDbContext(DbContextOptions options) : DbContext(options)
+{
+    public DbSet<ProductEntity> Products => Set<ProductEntity>();
+    public DbSet<CartEntity> Carts => Set<CartEntity>();
+    public DbSet<PositionEntity> Positions => Set<PositionEntity>();
+    public DbSet<OrderEntity> Orders => Set<OrderEntity>();
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        builder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+
+        base.OnModelCreating(builder);
+    }
+}
---
+++ src/Persistence/DependencyInjection.cs
@@ -0,0 +1,23 @@
+﻿using System.Reflection;
+
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+using ShopOfPryaniks.Persistence.Data;
+
+namespace ShopOfPryaniks.Persistence;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddPersistenceServices(this IServiceCollection services)
+    {
+        services.AddAutoMapper(Assembly.GetExecutingAssembly());
+
+        services.AddDbContext<PryanikiDbContext>(options =>
+        {
+            options.UseInMemoryDatabase("Pryaniki");
+        });
+
+        return services;
+    }
+}
