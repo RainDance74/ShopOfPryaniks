911e91324a474e42df4f90b28924a0fab3916832
RainDance74 <104539834+RainDance74@users.noreply.github.com>
2024-01-31 16:17:48 +0500

    Add dependency injection to application

---
+++ src/Application/DependencyInjection.cs
@@ -0,0 +1,20 @@
+﻿using System.Reflection;
+
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ShopOfPryaniks.Application;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+    {
+        services.AddAutoMapper(Assembly.GetExecutingAssembly());
+
+        services.AddMediatR(cfg =>
+        {
+            cfg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly());
+        });
+
+        return services;
+    }
+}
