6fb03e021c98199543cb14353768f96b3b59b0da
RainDance74 <104539834+RainDance74@users.noreply.github.com>
2024-01-31 14:51:39 +0500

    Add persistence layer

---
+++ src/Persistence/Entities/OrderEntity.cs
@@ -0,0 +1,30 @@
+﻿using AutoMapper;
+
+using ShopOfPryaniks.Domain.Common;
+using ShopOfPryaniks.Domain.Entities;
+
+namespace ShopOfPryaniks.Persistence.Entities;
+
+public class OrderEntity : BaseEntity
+{
+    public string ClientPhone { get; set; } = default!;
+    public OrderEntityStatus Status { get; set; }
+    public List<PositionEntity> Positions { get; } = [];
+
+    private class Mapping : Profile
+    {
+        public Mapping()
+        {
+            CreateMap<Order, OrderEntity>();
+            CreateMap<OrderEntity, Order>();
+        }
+    }
+}
+
+public enum OrderEntityStatus
+{
+    Done,
+    Delivery,
+    Canceled,
+    Burned
+}
