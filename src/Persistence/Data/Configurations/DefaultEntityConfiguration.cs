1e14a0842a9ca2dbce7f900a3cb4f0dde1f96420
RainDance74 <104539834+RainDance74@users.noreply.github.com>
2024-02-01 09:21:51 +0500

    Add entities configuration

---
+++ src/Persistence/Data/Configurations/DefaultEntityConfiguration.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+using ShopOfPryaniks.Domain.Common;
+
+namespace ShopOfPryaniks.Persistence.Data.Configurations;
+
+public static class DefaultEntityConfiguration
+{
+    public static void ConfigureBase<TEntity>(this EntityTypeBuilder<TEntity> builder)
+        where TEntity : BaseEntity
+    {
+        builder
+            .HasKey(x => x.Id);
+    }
+}
---
+++ src/Persistence/Data/Configurations/OrderConfiguration.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+using ShopOfPryaniks.Domain.Entities;
+
+namespace ShopOfPryaniks.Persistence.Data.Configurations;
+public class OrderConfiguration : IEntityTypeConfiguration<Order>
+{
+    public void Configure(EntityTypeBuilder<Order> builder) => builder.ConfigureBase();
+}
