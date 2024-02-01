ed59046156a3a872faf45b06ea6488bf5b29384c
RainDance74 <104539834+RainDance74@users.noreply.github.com>
2024-02-01 09:23:09 +0500

    Replace in memory database with PostgreSQL

--- src/Persistence/DependencyInjection.cs
+++ src/Persistence/DependencyInjection.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 using ShopOfPryaniks.Application.Common.Interfaces;
@@ -8,11 +9,20 @@ namespace ShopOfPryaniks.Persistence;
 
 public static class DependencyInjection
 {
-    public static IServiceCollection AddPersistenceServices(this IServiceCollection services)
+    public static IServiceCollection AddPersistenceServices(this IServiceCollection services, IConfiguration configuration)
     {
+        var connectionString = configuration.GetConnectionString("DefaultConnection");
+
+        if(string.IsNullOrEmpty(connectionString))
+        {
+            throw new ArgumentNullException(nameof(connectionString), "You must specify \"DefaultConnection\" in the configuration first.");
+        }
+
         services.AddDbContext<PryanikiDbContext>(options =>
         {
-            options.UseInMemoryDatabase("Pryaniki");
+            options
+                .UseNpgsql(connectionString)
+                .UseSnakeCaseNamingConvention();
         });
 
         services.AddScoped<IPryanikiDbContext>(provider => provider.GetRequiredService<PryanikiDbContext>());
--- src/Web/Program.cs
+++ src/Web/Program.cs
@@ -1,12 +1,13 @@
 using ShopOfPryaniks.Application;
 using ShopOfPryaniks.Persistence;
+using ShopOfPryaniks.Persistence.Data;
 using ShopOfPryaniks.Web;
 
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddApplicationServices();
-builder.Services.AddPersistenceServices();
+builder.Services.AddPersistenceServices(builder.Configuration);
 builder.Services.AddWebServices();
 
 WebApplication app = builder.Build();
@@ -14,6 +15,7 @@ WebApplication app = builder.Build();
 // Configure the HTTP request pipeline.
 if(app.Environment.IsDevelopment())
 {
+    await app.InitialiseDatabaseAsync();
     app.UseSwagger();
     app.UseSwaggerUI();
 }
