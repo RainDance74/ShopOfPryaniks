294b4054572ad2d4f5ed0f06845fc46d0a6e2fdb
RainDance74 <104539834+RainDance74@users.noreply.github.com>
2024-02-01 15:09:22 +0500

    Add result model to application

---
+++ src/Application/Common/Models/Result.cs
@@ -0,0 +1,18 @@
+﻿namespace ShopOfPryaniks.Application.Common.Models;
+
+public class Result
+{
+    internal Result(bool succeeded, IEnumerable<string> errors)
+    {
+        Succeeded = succeeded;
+        Errors = errors.ToArray();
+    }
+
+    public bool Succeeded { get; set; }
+
+    public string[] Errors { get; set; }
+
+    public static Result Success() => new(true, Array.Empty<string>());
+
+    public static Result Failure(IEnumerable<string> errors) => new(false, errors);
+}
