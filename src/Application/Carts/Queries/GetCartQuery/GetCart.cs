8e6f85acc93052e42cb19b13222927b8cb83c88b
RainDance74 <104539834+RainDance74@users.noreply.github.com>
2024-02-01 08:34:12 +0500

    Add GetCartQuery
    Forgot about this one..

---
+++ src/Application/Carts/Queries/GetCartQuery/GetCart.cs
@@ -0,0 +1,24 @@
+﻿using AutoMapper;
+
+using MediatR;
+
+using ShopOfPryaniks.Application.Common.Interfaces;
+
+namespace ShopOfPryaniks.Application.Carts.Queries.GetCartQuery;
+public record GetCartQuery : IRequest<CartVM>;
+
+public class GetCartQueryHandler(
+    IPryanikiDbContext context, IMapper mapper)
+    : IRequestHandler<GetCartQuery, CartVM>
+{
+    private readonly IPryanikiDbContext _context = context;
+    private readonly IMapper _mapper = mapper;
+
+    public async Task<CartVM> Handle(GetCartQuery request, CancellationToken cancellationToken)
+    {
+        return new CartVM
+        {
+
+        };
+    }
+}
