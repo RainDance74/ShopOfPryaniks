dbe24d908aaffc1482671cd1071815a41ebfafdf
RainDance74 <104539834+RainDance74@users.noreply.github.com>
2024-02-02 11:34:20 +0500

    Remove client phone from DTOs

--- src/Application/Orders/Queries/GetOrders/OrderDTO.cs
+++ src/Application/Orders/Queries/GetOrders/OrderDTO.cs
@@ -7,7 +7,6 @@ namespace ShopOfPryaniks.Application.Orders.Queries.GetOrders;
 public class OrderDTO
 {
     public int Id { get; init; }
-    public string ClientPhone { get; init; } = default!;
     public List<OrderPositionDTO> Positions { get; init; } = [];
     public OrderStatus Status { get; init; }
     public decimal PriceTotal { get; init; }
